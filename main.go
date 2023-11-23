36e3667d8467c46e4a5503a3c2be1b73ed07fe7c
Mikhail <m.alpinskiy@vk.team>
2023-11-23 15:01:58 +0300

    sleep

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 	"time"
@@ -11,13 +12,14 @@ import (
 func main() {
 	m := map[statshouse.Tags]int{}
 	for i := 0; i < 10; i++ {
-		for j := 0; j < 2; j++ {
+		for j := 0; j < 10; j++ {
 			m[statshouse.Tags{
-				1: strconv.Itoa(i + 1),
+				1: strconv.Itoa(10 * (i + 1)),
 				2: strconv.Itoa(j + 1),
-			}] = (i + 1) * (j + 1)
+			}] = 10*(i+1) + (j + 1)
 		}
 	}
+	fmt.Printf("%d\n", len(m))
 	for {
 		v := float64(math.Sin(2 * math.Pi * float64(time.Now().Second()) / 60.))
 		for k, n := range m {
@@ -25,5 +27,6 @@ func main() {
 				statshouse.Metric("load_generator_value", k).Value(v)
 			}
 		}
+		time.Sleep(time.Nanosecond)
 	}
 }
